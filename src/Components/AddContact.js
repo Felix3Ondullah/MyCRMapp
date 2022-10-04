c5e62077042b92484526c7376bee4fca829b68bc
Felix Ondullah <felixondullah@gmail.com>
2022-10-04 12:52:31 +0300

    added states

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -1,25 +1,51 @@
 import React from "react";
 
-const AddContact = () => {
+class AddContact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    number: "",
+  };
+  render(){
   return (
     <div className="ui main">
       <h2> Add Contact</h2>
       <form className="ui form">
         <div className="field">
           <label> Customer's Name</label>
-          <input type="text" name="name" placeholder="Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={(e) => this.setState({ name: e.target.value })}
+          />
         </div>
         <div className="field">
           <label>Customer's Email</label>
-          <input type="text" name="email" placeholder="Email" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
         </div>
         <div className="field">
           <label>Customer's Number</label>
-          <input type="text" name="number" placeholder="Number" />
+          <input
+            type="text"
+            name="number"
+            placeholder="Number"
+            value={this.state.number}
+            onChange={(e) => this.setState({ number: e.target.value })}
+          />
+         
         </div>
         <button className="ui button red"> Add</button>
       </form>
     </div>
   );
 };
+}
 export default AddContact;
