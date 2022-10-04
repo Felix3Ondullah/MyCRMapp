fc5bc04844143d2522afb41db26324c732e6c740
Felix Ondullah <felixondullah@gmail.com>
2022-10-04 11:51:43 +0300

    added style and header

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Header(){
+    return(
+        <div className="ui fixed menu">
+            <div className = "ui container center">
+                <h2>Customer Relationship Manager</h2>
+
+            </div>
+
+        </div>
+    );
+
+    
+}
+export default Header;
