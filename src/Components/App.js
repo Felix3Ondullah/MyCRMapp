9e5df04d65f605ac3b0fad3a944b4ff0b9e2baa2
Felix Ondullah <felixondullah@gmail.com>
2022-10-04 11:46:07 +0300

    worked on root components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
 
+const App =() =>{
+ const contacts =[
+  {
+    id:"1",
+    name:"Rosland Andisi",
+    email: "roselandandisi@gmail.com",
+    number: "0714005569",
+  },
+  {
+    id:"2",
+    name:"Elvis Gatukui",
+    email: "elvisgatukui@gmail.com",
+    number: "0714695569",
+  },
+  {
+    id:"3",
+    name:"Felix Ondullah",
+    email: "felixondullah@gmail.com",
+    number: "0714175569",
+  }
+ ];
 
-function App() {
   return (
-    <div>Hello World</div>
-  );
+    <div className="ui container">
+      < Header/>
+      < ContactList contacts={contacts}/>
+      < AddContact/>
+    </div>
+    );
+  
 }
 
 export default App;
