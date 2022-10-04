b579294994c841ac3b62f8de644433c23cfc1619
Felix Ondullah <felixondullah@gmail.com>
2022-10-04 12:52:57 +0300

    added states

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,29 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import "./App.css"
 import Header from "./Header";
 import ContactList from "./ContactList";
 import AddContact from "./AddContact";
 
 const App =() =>{
- const contacts =[
-  {
-    id:"1",
-    name:"Rosland Andisi",
-    email: "roselandandisi@gmail.com",
-    number: "0714005569",
-  },
-  {
-    id:"2",
-    name:"Elvis Gatukui",
-    email: "elvisgatukui@gmail.com",
-    number: "0714695569",
-  },
-  {
-    id:"3",
-    name:"Felix Ondullah",
-    email: "felixondullah@gmail.com",
-    number: "0714175569",
-  }
- ];
+  const[contacts,setContacts]=useState([]);
 
   return (
     <div className="ui container">
