883b3017d699880265f69ec3a0af674c75a08e92
Felix Ondullah <felixondullah@gmail.com>
2022-10-04 11:47:25 +0300

    added labels

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -1,1 +1,25 @@
+import React from "react";
 
+const AddContact = () => {
+  return (
+    <div className="ui main">
+      <h2> Add Contact</h2>
+      <form className="ui form">
+        <div className="field">
+          <label> Customer's Name</label>
+          <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div className="field">
+          <label>Customer's Email</label>
+          <input type="text" name="email" placeholder="Email" />
+        </div>
+        <div className="field">
+          <label>Customer's Number</label>
+          <input type="text" name="number" placeholder="Number" />
+        </div>
+        <button className="ui button red"> Add</button>
+      </form>
+    </div>
+  );
+};
+export default AddContact;
