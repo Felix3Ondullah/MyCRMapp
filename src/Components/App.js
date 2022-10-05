9e2fb9e70dcf13dfe89ded8a83dcf287fbac0a24
Felix Ondullah <felixondullah@gmail.com>
2022-10-05 11:42:42 +0300

    installed and imported axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,14 +4,25 @@ import "./App.css";
 import Header from "./Header";
 import ContactList from "./ContactList";
 import AddContact from "./AddContact";
+import api from "../api/contacts";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+//retrieve contacts
+const retrieveContacts = async() => {
+  const response = await api.get ("/contacts");
+  return response.data;
+};
+
+
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, { id: nanoid(), ...contact }]);
   };
+
   const removeContactHandler = (id) => {
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
@@ -20,17 +31,24 @@ const App = () => {
   };
 
   useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts);
+    // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if (retriveContacts) setContacts(retriveContacts);
+    const getAllContacts =async () => {
+      const allContacts = await retrieveContacts();
+      if(allContacts) setContacts (allContacts);
+    }
+    getAllContacts();
   }, []);
+
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
       <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 };
