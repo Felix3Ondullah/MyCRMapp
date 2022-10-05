5119339a15b3f7e79a93a57b2739531ec2e46345
Felix Ondullah <felixondullah@gmail.com>
2022-10-05 10:54:34 +0300

    changed header style

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
   //console.log(props)
-  const deleteContactHandler = (id)=> {
-    props.getContactId(id)
-  }
-  const renderContactList = props.contacts.map((contact) => {
-    return <ContactCard contact={contact} clickHandler={deleteContactHandler} key ={contact.id} />;
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const contacts = [
+    {
+      id: "1",
+      "name": "Rosland Andisi",
+      "email": "roselandandisi@gmail.com",
+      "number": "0714005569",
+    },
+  ];
+  const renderContactList = contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      />
+    );
   });
 
-  return <div className="ui celled list"> {renderContactList}</div>;
+  return (
+    <div class="main">
+      <h3> Contact list </h3>
+      <div className="ui celled list"> {renderContactList}</div>
+    </div>
+  );
 };
 export default ContactList;
