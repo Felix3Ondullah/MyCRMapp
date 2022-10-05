f091877769bab4a9845f3f62bbff964481395eaf
Felix Ondullah <felixondullah@gmail.com>
2022-10-05 16:08:49 +0300

    added search functionality

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,21 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useRef} from "react";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
   //console.log(props)
+  const inputElement = useRef("");
   const deleteContactHandler = (id) => {
     props.getContactId(id);
   };
-  const contacts = [
-    {
-      id: "1",
-      "name": "Rosland Andisi",
-      "email": "roselandandisi@gmail.com",
-      "number": "0714005569",
-    },
-  ];
-  const renderContactList = contacts.map((contact) => {
+
+  const renderContactList = props.contacts.map((contact) => {
     return (
       <ContactCard
         contact={contact}
@@ -25,9 +18,28 @@ const ContactList = (props) => {
     );
   });
 
+  const getSearchTerm = () => {
+    // console.log(inputElement);
+    props.searchKeyword(inputElement.current.value);
+  };
+
+
   return (
     <div class="main">
       <h3> Contact list </h3>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+           ref = {inputElement}
+            type="text"
+            placeholder="Search Contacts"
+            className="prompt"
+            value={props.term}
+            onChange={getSearchTerm}
+          />
+          <i className="search icon"></i>
+        </div>
+      </div>
       <div className="ui celled list"> {renderContactList}</div>
     </div>
   );
