909386a6c9b77689b30ea80f94d6c2ad4c7df32c
Felix Ondullah <felixondullah@gmail.com>
2022-10-05 16:09:24 +0300

    added search functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,32 +6,32 @@ import ContactList from "./ContactList";
 import AddContact from "./AddContact";
 import api from "../api/contacts";
 
-
 const App = () => {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  //retrieve contacts
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  };
 
-//retrieve contacts
-const retrieveContacts = async() => {
-  const response = await api.get ("/contacts");
-  return response.data;
-};
-
-
-  const addContactHandler = async(contact) => {
+  const addContactHandler = async (contact) => {
     console.log(contact);
     const request = {
-      id:nanoid(),
-      ...contact
+      id: nanoid(),
+      ...contact,
     };
 
-
-   const response =  await api.post ("./contacts" ,request)
-   console.log(response);
+    const response = await api.post("./contacts", request);
+    console.log(response);
     setContacts([...contacts, response.data]);
   };
 
-  const removeContactHandler = async(id) => {
+  // const updateContactHandler =() => {};
+
+  const removeContactHandler = async (id) => {
     await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
@@ -39,16 +39,33 @@ const retrieveContacts = async() => {
     setContacts(newContactList);
   };
 
+  const searchHandler = (searchTerm) => {
+    // console.log(searchTerm)
+    setSearchTerm(searchTerm);
+    if (searchTerm !== "") {
+      const newContactList = contacts.filter((contact) => {
+       return Object.values(contact)
+          .join(" ")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      });
+      setSearchResults(newContactList);
+    }
+    else {
+      setSearchResults(contacts);
+    }
+  };
+
   useEffect(() => {
     // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     // if (retriveContacts) setContacts(retriveContacts);
     const getAllContacts = async () => {
       const allContacts = await retrieveContacts();
-      console.log(allContacts)
-      if(allContacts) setContacts (allContacts);
-    }
+      console.log(allContacts);
+      if (allContacts) setContacts(allContacts);
+    };
     getAllContacts();
-    console.log(getAllContacts)
+    console.log(getAllContacts);
   }, []);
 
   useEffect(() => {
@@ -59,7 +76,12 @@ const retrieveContacts = async() => {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <ContactList
+        contacts={contacts}
+        getContactId={removeContactHandler}
+        term={searchTerm}
+        searchKeyword={searchHandler}
+      />
     </div>
   );
 };
